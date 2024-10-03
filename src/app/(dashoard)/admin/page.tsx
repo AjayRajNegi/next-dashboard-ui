3ef878850a21ca187136ae7b130bc9bfd527a49b
Ajay <ajayrajnegi111@gmail.com>
2024-10-03 10:25:32 +0530

    Major changes in the frontend.

--- src/app/(dashoard)/admin/page.tsx
+++ src/app/(dashoard)/admin/page.tsx
@@ -1,11 +1,13 @@
+import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 export default function AdminPage() {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
+    <div className="p-4 flex gap-4 flex-col md:flex-row bg-[#F7F8FA]">
       {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
@@ -32,7 +34,10 @@ export default function AdminPage() {
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 }
